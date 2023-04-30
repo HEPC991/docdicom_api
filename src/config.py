b58e5bf8bb0c7b108f9e44f568ce5a2cab54eb22
JorshBG <jordiyair29@gmail.com>
2023-04-30 13:01:21 -0600

    Asignar modo asociativo a las consultas SQL

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ class DevelopmentConfig():
     DEBUG = True
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'Myw1n5ql#09'
+    MYSQL_PASSWORD = 'Alucard29+'
     MYSQL_DB = 'dicom'
 
 config = {
