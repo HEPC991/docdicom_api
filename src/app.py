8f674e0d0ad3a4867c53db7ec81ecec2f649e78d
JorshBG <jordiyair29@gmail.com>
2023-04-30 23:50:27 -0600

    Agregar procedimeintos de almacenado

--- src/app.py
+++ src/app.py
@@ -3,10 +3,11 @@ from flask import Flask,jsonify, request, send_file
 from flask_mysqldb import MySQL
 from flask_cors import CORS
 from config import config
-
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+
+bcrypt = Bcrypt(app)
 
 CORS(app)
 
@@ -17,18 +18,25 @@ conexion =MySQL(app)
 def get_usuarios():
     try:
         cursor= conexion.connection.cursor()
-        cursor.callproc('get_all_users')
+        cursor.callproc('sp_get_users')
         datos = cursor.fetchall()
-        return jsonify({'usuarios':datos, 'mensaje':'usuarios listados.'}), 200
+        if datos is not None:
+            if len(datos) > 0:
+                return jsonify({'usuarios':datos, 'mensaje':'usuarios listados.'}), 200
+            else:
+                return jsonify({'mensaje':'No hay usuarios registrados.'}), 404
+        else: 
+            return jsonify({'mensaje':'No hay usuarios registrados.'}), 404
     except Exception as ex:
-        return jsonify({'mensaje':'Error de la peticion.'}), 400
+        return jsonify({'mensaje':'Error de la peticion.'}), 500
+
 
 # REgistrar un estudio medico
 @app.post('/api/register/medic_study')
 def registrar_estudio():
     try:
         cursor = conexion.connection.cursor()
-        cursor.callproc('register_medic_study', [
+        cursor.callproc('sp_register_medic_study', [
             request.json['ms_date_study'], 
             request.json['ms_description'], 
             request.json['ms_status'], 
@@ -37,65 +45,90 @@ def registrar_estudio():
         conexion.connection.commit()
         return jsonify({'mensaje':'Estudio registrado.'}), 201
     except Exception as ex:
-        return jsonify({'mensaje':'Error de conexion.'}), 400
+        return jsonify({'mensaje':ex}), 500
 
 # Validacion del usuario
 @app.post('/api/validate/user')
 def validar_usuarios():
     try:
         cursor = conexion.connection.cursor()
-        cursor.callproc('validate_login', [request.json['u_email']])
-        fila = cursor.fetchone()
-        if fila is not None:
-            usuario = {
-                'u_email': fila[0],
-                'u_password': fila[1],
-            }
-            if usuario['u_password'] == request.json['u_password']:
-                return jsonify({'mensaje': 'Usuario validado.', 'usuario': usuario['u_email']})
+        cursor.callproc('sp_validate_user', [request.json['u_email']])
+        usuario = cursor.fetchone()
+        if usuario is not None:
+            if bcrypt.check_password_hash(usuario['password'], request.json['u_password']):
+                # remove the password field from the json to return it to the client
+                del usuario['password']
+                return jsonify({'mensaje': 'Usuario validado.', 'usuario': usuario})
             else:
                 return jsonify({"mensaje":"Usuario invalido, la contraseña no corresponde."})
         else:
             return jsonify({'mensaje': 'El usuario no existe.'})
     except Exception as ex:
-        return jsonify({'mensaje': 'Error al validar el usuario: {}'.format(str(ex))})      
+        return jsonify({'Mensaje de error': ex}), 500
+
+@app.get('/api/test/number/<number>')
+def test(number):
+    try:
+        return jsonify({'message': int(number)})
+    except ValueError as ex:
+        return jsonify({'mensaje':'Error de conversion.'})
+
 
 # Registrar un paciente
-@app.post('/api/register/<paciente>')
-def registrar_usuario(paciente):
+@app.post('/api/register/<role>')
+def registrar_usuario(role):
     try:
-        paciente = int(paciente)
+        try:
+            role = int(role)
+        except ValueError as nex:
+            return jsonify({'Bad request': 'Bad role or activate'}), 400
         cursor = conexion.connection.cursor()
-        if paciente == 10000000:
-            sql = "INSERT INTO users (u_email, u_name, u_last_name, u_last_m_name, u_password, u_phone, u_status, u_s_id, u_r_id) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}')".format(
-                request.json['u_email'], request.json['u_name'], request.json['u_last_name'],
-                request.json['u_last_m_name'], request.json['u_password'], request.json['u_phone'],
-                request.json['u_status'], request.json['u_s_id'], int(paciente)
-            )
-        elif paciente == 10000001:
-            sql = "INSERT INTO users (u_email, u_name, u_last_name, u_last_m_name, u_password, u_phone, u_status, u_s_id, u_r_id) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}')".format(
-                request.json['u_email'], request.json['u_name'], request.json['u_last_name'],
-                request.json['u_last_m_name'], request.json['u_password'], request.json['u_phone'],
-                request.json['u_status'], request.json['u_s_id'], paciente
-            )
-        elif paciente == 10000002:
-            sql = "CALL insert_patient('{0}', '{1}', '{2}', '{3}', '{4}', '{5}')".format(
-                request.json['u_email'], request.json['u_name'], request.json['u_last_name'],
-                request.json['u_last_m_name'], request.json['u_password'], request.json['u_phone']
-            )
-        cursor.execute(sql)
+        # admin and medic
+        try:
+            flag = True
+            cursor.callproc('sp_validate_user', [request.json['u_email']])
+            user = cursor.fetchone()
+            if user is not None:
+                flag = False
+            if flag:
+                if role == 10000000 or role == 10000001:
+                    cursor.callproc('sp_create_user',[
+                        request.json['u_email'], 
+                        request.json['u_name'], 
+                        request.json['u_last_name'],
+                        request.json['u_last_m_name'], 
+                        bcrypt.generate_password_hash(request.json['u_password']), 
+                        request.json['u_phone'],
+                        request.json['u_status_p'],
+                        request.json['u_s_id'],
+                        int(role)
+                    ])
+                elif role == 10000002:
+                    cursor.callproc('sp_create_patient', [
+                        request.json['u_email'], 
+                        request.json['u_name'], 
+                        request.json['u_last_name'],
+                        request.json['u_last_m_name'], 
+                        bcrypt.generate_password_hash(request.json['u_password']), 
+                        request.json['u_phone']
+                    ])
+                else: 
+                    return jsonify({'Bad request': 'Role not found'})
+            else:
+                return jsonify({'Bad request': 'User already exists'})
+        except Exception as uex:
+            return jsonify({})
         conexion.connection.commit()
         return jsonify({'mensaje':'Usuario registrado.'})
     except Exception as ex:
-        return jsonify({'mensaje':'Error de conexion.'})
+        return jsonify({'mensaje':ex})
 
 # Activar un usuario
 @app.put('/api/activate/<u_id>')
 def activar_usuario(u_id):
     try:
         cursor = conexion.connection.cursor()
-        sql = "UPDATE users SET u_status = '{0}' WHERE u_id = '{1}'".format(request.json['u_status'],u_id)
-        cursor.execute(sql)
+        cursor.callproc('sp_activate_user', [u_id])
         conexion.connection.commit()
         return jsonify({'mensaje':'Usuario activado.'})
     except Exception as ex:
@@ -106,8 +139,7 @@ def activar_usuario(u_id):
 def desactivar_usuario(u_id):
     try:
         cursor = conexion.connection.cursor()
-        sql = "UPDATE users SET u_status = '{0}' WHERE u_id = '{1}'".format(request.json['u_status'],u_id)
-        cursor.execute(sql)
+        cursor.callproc('sp_deactivate_user', [u_id])
         conexion.connection.commit()
         return jsonify({'mensaje':'Usuario desactivado.'})
     except Exception as ex:
@@ -118,30 +150,25 @@ def desactivar_usuario(u_id):
 def registrar_instituto():
     try:
         cursor = conexion.connection.cursor()
-        sql = "INSERT INTO medical_institutions (mi_institute, mi_address, mi_status) VALUES ('{0}', '{1}', '{2}')".format(
-            request.json['mi_institute'], request.json['mi_address'], request.json['mi_status'])
-        cursor.execute(sql)
+        cursor.callproc('sp_register_institute', [
+            request.json['mi_name'],
+            request.json['mi_address']
+        ])
         conexion.connection.commit()
         return jsonify({'mensaje':'Instituto registrado.'})
     except Exception as ex:
         return jsonify({'mensaje':'Error de conexion.'})
     
 # Listar los estudio medicos
-@app.route('/api/get/medic_study/all', methods = ['GET'])
+@app.get('/api/get/medic_study/all')
 def get_estudios():
     try:
         cursor= conexion.connection.cursor()
-        sql = "Select ma_date_study, ma_description, ma_status, ma_mi_id, ma_cs_id FROM medical_appoiment"
-        cursor.execute(sql)
+        cursor.callproc('sp_get_medic_study')
         datos = cursor.fetchall()
-        usuarios = []
-        for fila in datos:
-            curso = {'ma_date_study':fila[0], 'ma_description':fila[1], 'ma_status':fila[2], 
-                     'ma_mi_id':fila[3], 'ma_cs_id':fila[4]}
-            usuarios.append(curso)
-        return jsonify({'usuarios':usuarios, 'mensaje':'estudios listados.'})
+        return jsonify({'mensaje':'estudios listados.', 'Estudios':datos})
     except Exception as ex:
-        return jsonify({'mensaje':'Error.'})
+        return jsonify({'mensaje':ex})
 """   
 @app.route('/api/get/dicom/all', methods=['GET'])
 def get_dicom():
@@ -177,14 +204,9 @@ def get_dicom():
 def get_institutos():
     try:
         cursor= conexion.connection.cursor()
-        sql = "Select mi_institute, mi_address, mi_status FROM medical_institutions"
-        cursor.execute(sql)
+        cursor.callproc('sp_get_institute')
         datos = cursor.fetchall()
-        usuarios = []
-        for fila in datos:
-            curso = {'mi_institute':fila[0], 'mi_address':fila[1], 'mi_status':fila[2]}
-            usuarios.append(curso)
-        return jsonify({'usuarios':usuarios, 'mensaje':'institutos listados.'})
+        return jsonify({'mensaje':'estudios listados.', 'Institutos':datos})
     except Exception as ex:
         return jsonify({'mensaje':'Error.'})
 
@@ -193,14 +215,9 @@ def get_institutos():
 def get_menus():
     try:
         cursor= conexion.connection.cursor()
-        sql = "Select m_title, m_url, m_status FROM menus"
-        cursor.execute(sql)
+        cursor.callproc('sp_get_menus')
         datos = cursor.fetchall()
-        usuarios = []
-        for fila in datos:
-            curso = {'m_title':fila[0], 'm_url':fila[1], 'm_status':fila[2]}
-            usuarios.append(curso)
-        return jsonify({'usuarios':usuarios, 'mensaje':'menus listados.'})
+        return jsonify({'mensaje':'estudios listados.', 'menus':datos})
     except Exception as ex:
         return jsonify({'mensaje':'Error.'})
 
@@ -209,9 +226,11 @@ def get_menus():
 def crear_menu():
     try:
         cursor = conexion.connection.cursor()
-        sql = "INSERT INTO menus (m_title, m_url, m_status) VALUES ('{0}', '{1}', '{2}')".format(
-            request.json['m_title'], request.json['m_url'], request.json['m_status'])
-        cursor.execute(sql)
+        cursor.callproc('sp_create_menu', [
+            request.json['m_title'],
+            request.json['m_url'],
+            request.json['m_status']
+        ])
         conexion.connection.commit()
         return jsonify({'mensaje':'Menu registrado.'})
     except Exception as ex:
@@ -266,17 +285,22 @@ def eleminar_usuario(id):
 def eleminar_menu(id):
     try:
         cursor = conexion.connection.cursor()
-        cursor.callproc('sp_delete_menu', [id])
+        cursor.callproc('sp_delete_menu', [
+            id,
+            request.remote_addr,
+            request.json['m_by_user']
+        ])
         conexion.connection.commit()
         return jsonify({'mensaje':'Menu eliminado.'}), 201
     except Exception as ex:
-        return jsonify({'mensaje':'Error de conexion.'}), 500
+        return jsonify({'mensaje':ex}), 500
 
-def pagina_no_encontrada(error):
-    return "<h1>La pagina que intentas buscar no existe</h1>",404
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'This page does not exist', 404
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
     app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-    app.register_error_handler(404, pagina_no_encontrada)
+    app.register_error_handler(404, page_not_found)
     app.run()
