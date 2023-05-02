2fcd7d2c496b392a5860b1aae5e37ffdbda28b47
JorshBG <jordiyair29@gmail.com>
2023-05-02 05:40:40 -0600

    Listar dicoms convertidos a png

--- src/routes/selects.py
+++ src/routes/selects.py
@@ -1,5 +1,12 @@
-from flask import Flask, jsonify, request, Blueprint
+from flask import Flask, jsonify, request, Blueprint, send_file, Response
+import pydicom
+from PIL import Image
+from io import BytesIO
 from database.db import conexion
+import base64
+import numpy as np
+import cv2
+import json
 
 select = Blueprint('select', __name__, url_prefix='/api/get/')
 
@@ -54,32 +61,35 @@ def get_menus():
     except Exception as ex:
         return jsonify({'mensaje':'Error.'})
     
-"""   
-@app.route('/api/get/dicom/all', methods=['GET'])
-def get_dicom():
-    try:
-        cursor = conexion.connection.cursor()
-        sql = "SELECT d_serie, d_dicom, d_status, d_ma_id FROM dicoms"
-        cursor.execute(sql)
-        datos = cursor.fetchone()
-        
-        return send_file(io.BytesIO(datos[1]), attachment_filename='dicom.dcm', as_attachment=True)
-    except Exception as ex:
-        return jsonify({'mensaje': 'Error.'})
-"""
-"""
-@app.route('/api/get/dicom/all', methods = ['GET'])
-def get_dicom():
+@select.get('image/<int:id_dicom>')
+def obtener_una_imagen_por_id(id_dicom):
     try:
         cursor= conexion.connection.cursor()
-        sql = "Select d_serie, d_status, d_ma_id, d_dicom FROM dicoms"
-        cursor.execute(sql)
-        datos = cursor.fetchall()
-        usuarios = []
-        for fila in datos:
-            curso = {'d_serie':fila[0], 'd_status':fila[1], 'd_ma_id':fila[2]}
-            usuarios.append(curso)
-        return jsonify({'usuarios':usuarios, 'mensaje':'Imagenes DICOM listadas.'})
+        cursor.callproc('sp_get_image', [id_dicom])
+        datos = cursor.fetchone()
+        imagen = datos['i_image']
+        imagen = base64.b64decode(imagen)
+        response = Response(imagen, mimetype='image/png')
+        serie = datos['i_d_serie']
+        response.headers.set('Content-Disposition', 'attachment', filename=serie+'.png')
+        return response
     except Exception as ex:
-        return jsonify({'mensaje':'Error.'})
-"""
+        return jsonify({'mensaje':ex}), 500
+    
+
+@select.get('images/<string:serie>')
+def obtner_imagenes_por_serie(serie):
+    try:
+        cursor= conexion.connection.cursor()
+        cursor.callproc('sp_get_images', [serie])
+        datos = cursor.fetchall()
+        imagenes = []
+        for dato in datos:
+            imagen = dato['i_image']
+            imagen = base64.b64decode(imagen)
+            imagen = base64.b64encode(imagen).decode('utf-8')
+            imagenes.append({'imagen':imagen})
+
+        return json.dumps(imagenes)
+    except Exception as ex:
+        return jsonify({'mensaje':ex}), 500
