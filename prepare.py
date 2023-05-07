948cfb6b7891dd8810249808730fb9e69ec4226f
JorshBG <jordiyair29@gmail.com>
2023-05-07 02:25:40 -0600

    Fix prepare.py

--- prepare.py
+++ prepare.py
@@ -1,5 +1,5 @@
 import os
 
 os.system("python -m virtualenv env")
-os.system("source env/bin/activate")
+os.system(os.path.join(os.getcwd(), "env", "Scripts", "activate"))
 os.system("pip install -r requirements.txt")
