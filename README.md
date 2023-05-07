8f11d1b5f53feca2031d1efb488368a956e61fd8
JorshBG <jordiyair29@gmail.com>
2023-05-07 03:08:00 -0600

    Fix config scripts

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Go to root project and run the next command
 
 ```bash
 python ./prepare.py
+./env/Scripts/activate
+python ./install.py
 ```
 
 Now, to serve the API run this:
--- prepare.py
+++ prepare.py
@@ -1,5 +1,3 @@
 import os
 
 os.system("python -m virtualenv env")
-os.system(os.path.join(os.getcwd(), "env", "Scripts", "activate"))
-os.system("pip install -r requirements.txt")
